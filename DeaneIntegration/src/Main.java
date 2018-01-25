84b2840c8419631bad4cc84f6fc004092c9fcf15
zrdeane0475 <zrdeane0475@L438R2S7.STUDENT.ad.fgcu.edu>
2018-01-25 18:38:09 -0500

    First commit

---
+++ DeaneIntegration/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// Zachary Deane
+// My project that integrates all the skills that I have learned.
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		Dog spot = new Dog();
+		spot.size = 30; 
+		spot.breed = "German Shepherd";
+		System.out.println(spot.breed);
+		Dog spuds = new Dog();
+		spuds.size = 40;
+	}
+}
+
+class Dog {
+	double size;
+	String breed;
+}
